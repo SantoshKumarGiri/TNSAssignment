58e7125f72828bd0315994881c8a123d953d0092
SantoshKumarGiri <83579416+SantoshKumarGiri@users.noreply.github.com>
2023-06-28 09:57:21 +0530

    First Project

---
+++ TNSAssignment/src/santosh/HelloDemo.java
@@ -0,0 +1,11 @@
+package santosh;
+
+public class HelloDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello\t");
+		System.out.println("Santosh");
+
+	}
+
+}
---
+++ TNSAssignment/src/santosh/ProductOfTwoNumber.java
@@ -0,0 +1,12 @@
+package santosh;
+import java.util.*;
+public class ProductOfTwoNumber {
+
+	public static void main(String[] args) {
+		System.out.println("Hello\t");
+		System.out.println("Santosh");
+		
+
+	}
+
+}
